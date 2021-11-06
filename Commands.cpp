b53b02f3b6f804422cc501ffcd75097ef78ddbb9
Itay Segev <itaysegev11@gmail.com>
2021-11-06 19:58:07 +0200

    small changes before first compilation

--- Commands.cpp
+++ Commands.cpp
@@ -144,7 +144,7 @@ Command * SmallShell::CreateCommand(const char* cmd_line) {
     return new ShowPidCommand(cmd_line);
   }
   if (firstWord.compare("cd") == 0) {
-    return new ChangeDirCommand(cmd_line,plastPwd);
+    return new ChangeDirCommand(cmd_line,SmallShell::plastPwd);
   }
   std::cout<<"ssss";
   // else {
