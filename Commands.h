05de2a984f74173a22b2470ab5a45a57e87eb325
Itay Segev <itaysegev11@gmail.com>
2021-11-08 21:23:02 +0200

    small changes before first compilation

--- Commands.h
+++ Commands.h
@@ -56,7 +56,7 @@ class ChangeDirCommand : public BuiltInCommand {
 // TODO: Add your data members
   char* plastPwd;
  public:
-  ChangeDirCommand(const char* cmd_line, char** plastPwd);
+  ChangeDirCommand(const char* cmd_line, char* plastPwd);
   virtual ~ChangeDirCommand() {}
   void execute() override;
 };
