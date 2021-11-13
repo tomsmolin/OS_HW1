09f3ca68c8d202c5f7a8b4b5dc1f5a8aeb8f36e3
Itay Segev <itaysegev11@gmail.com>
2021-11-13 21:55:54 +0200

    JobList implement and some of his methods

--- Commands.cpp
+++ Commands.cpp
@@ -200,7 +200,7 @@ void ChangeDirCommand::execute() {
 
 /////////////////////////////joblist//////////////////////
 
-JobsList::JobEntry::JobEntry(int pid, int job_pid,JobStatus status,time_t insert,const char* cmd) : pid(pid),job_pid(job_pid),status(status),insert(insert),cmd(cmd) {}
+JobsList::JobEntry::JobEntry(int pid, int job_id,JobStatus status,time_t insert,const char* cmd) : pid(pid),job_id(job_id),status(status),insert(insert),cmd(cmd) {}
 
 JobsList::JobsList() {}
 
