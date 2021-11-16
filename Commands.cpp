d07e70a7ebc92def3b5e84df10f67f5275a8ab0b
Itay Segev <itaysegev11@gmail.com>
2021-11-16 18:16:38 +0200

    kill command

--- Commands.cpp
+++ Commands.cpp
@@ -277,7 +277,7 @@ void KillCommand::execute() {
     std:: string str2 = args[2];
     std::string str3 = " does not exist";
     str.append(str2).append(str3);
-    perror((char*)str);
+    perror(str.c_str());
     return;
   }
   pid_t pid = curr_job->pid;
