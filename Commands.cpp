096241b8b6f927dda1ed76ae1e1246c5b27a607f
Itay Segev <itaysegev11@gmail.com>
2021-11-16 18:10:04 +0200

    kill command

--- Commands.cpp
+++ Commands.cpp
@@ -273,7 +273,10 @@ void KillCommand::execute() {
   job_id >> id;
   JobsList::JobEntry* curr_job = jobs->getJobById(id);
   if(curr_job == nullptr){
-    std::string str = "smash error: kill:job_id" + args[2] + "does not exist";
+    std::string str = "smash error: kill:job_id"; 
+    std:: string str2 = args[2];
+    std::string str3 = "does not exist";
+    str.append(str2).append(str3);
     std::cout << str << std::endl;
     perror("smash error: kill:job_id does not exist");
     return;
