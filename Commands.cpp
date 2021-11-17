1af12e0585ff377718e3207bacd32ce759b1ae14
Itay Segev <itaysegev11@gmail.com>
2021-11-17 22:10:45 +0200

    ctrC handler

--- Commands.cpp
+++ Commands.cpp
@@ -273,11 +273,11 @@ void KillCommand::execute() {
   job_id >> id;
   JobsList::JobEntry* curr_job = jobs->getJobById(id);
   if(curr_job == nullptr){
-    // std::string str = "smash error: kill:job_id "; 
+    std::string str = "smash error: kill:job_id "; 
     // std::string str2 = args[2];
     // std::string str3 = " does not exist\n";
     // str.append(str2).append(str3);
-    // fprintf(stderr,str.c_str());
+    fprintf(stderr,str.c_str());
     // curr_job = jobs->getJobById(0);
     return;
   }
