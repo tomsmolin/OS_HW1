081f381481324c2b6f2fac2de6ab56f597db63d4
Itay Segev <itaysegev11@gmail.com>
2021-11-25 11:29:37 +0200

    Testing

--- Commands.cpp
+++ Commands.cpp
@@ -627,7 +627,6 @@ void JobsList::addJob(int pid,std::string cmd, bool isStopped) {
   removeFinishedJobs();
   JobStatus curr_status = (isStopped) ?  Stopped : Background;
   jobs_list_empty=false;
-  free_job_id++;
   jobsDict[free_job_id] = JobEntry(pid, free_job_id,curr_status,time(NULL),cmd);
 }
 
