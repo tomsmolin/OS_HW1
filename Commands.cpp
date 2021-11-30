616e604662195af7c9b97fbe52732e043d2ab06e
Tom Smolin <tomsmo016@gmail.com>
2021-11-30 09:47:49 +0200

    HeadCommand: GDB

--- Commands.cpp
+++ Commands.cpp
@@ -638,6 +638,7 @@ void HeadCommand::execute() {
             seeker = 0;
         }
     }
+    write(1, '\0', 1);
     if (close(fd) == ERROR)
     {
         fprintf(stderr, "smash error: close failed\n");
