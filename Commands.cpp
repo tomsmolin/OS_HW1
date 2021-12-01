2e0d97ee26a5a28cac9c6cd34de8835c5be9ec19
Tom Smolin <tomsmo016@gmail.com>
2021-12-01 11:06:23 +0200

    cd: & at the parameter tweak

--- Commands.cpp
+++ Commands.cpp
@@ -318,6 +318,7 @@ void ChangeDirCommand::execute() {
     }
 
     char* path = args[1];
+    cout << path << endl;
     if (strcmp(path, "-") == 0)
     {
         if (classPlastPwd == NULL) // When last working directory isn't set on smash
