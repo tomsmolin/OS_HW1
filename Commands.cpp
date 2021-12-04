65f27fceb106bfc0f8f3a4f050e345acedc135fa
Tom Smolin <tomsmo016@gmail.com>
2021-12-04 12:52:26 +0200

    removing initialization warnings

--- Commands.cpp
+++ Commands.cpp
@@ -112,7 +112,7 @@ static int numberOfArgs(std::string cmd_line) {
     return cnt;
 }
 
-Command::Command(const char* cmd_line) : cmd(cmd_line), argv(0), timed_entry(NULL), cmd_job_id(NOT_SET) {  
+Command::Command(const char* cmd_line) : cmd(cmd_line), argv(0), cmd_job_id(NOT_SET), timed_entry(NULL) {
   args = new char*[numberOfArgs(cmd_line) + 1]; //buffer of (+1) due to impl. of _parse command
   argv = _parseCommandLine(cmd_line,args);
 }
