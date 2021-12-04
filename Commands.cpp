5b11f157f9c6a129409006c8bfa0428977d49833
Itay Segev <itaysegev11@gmail.com>
2021-12-04 15:20:23 +0200

    pipe showpid fixing

--- Commands.cpp
+++ Commands.cpp
@@ -1143,7 +1143,12 @@ void PipeCommand::execute() {
       perror("smash error: close failed");
       exit(0);
     }
-  
+    std::string cmd_s = _trim(string(second_command));
+    std::string firstWord = cmd_s.substr(0, cmd_s.find_first_of(" \n")); 
+    if (firstWord.compare("showpid") == 0 || firstWord.compare("showpid&") == 0) {
+      cout << "smash pid is " << curr_pid << endl;
+      exit(0);
+    }
     SmallShell::getInstance().executeCommand(second_command.c_str());
     exit(0);
   }
